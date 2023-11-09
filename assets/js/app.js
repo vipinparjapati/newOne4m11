6feb7c5e9d1b4515ddf7eb9c884c5cbbb8e18a4c
vipinparjapati <vipinvipin10198@gmail.com>
2023-11-09 16:47:56 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+function openNav() {
+    document.getElementById("navbar").classList.toggle("end-0");
+    document.body.classList.toggle("overflow-hidden");
+    document.getElementById("menubar-icon").classList.toggle("cross");
+  }
